a5f4f492b5aa6b3699d6228e2de24c627c418071
Faris Han <farishanugraha@gmail.com>
2021-10-17 00:33:24 +0700

    Fix posts on mobile (#5)

--- components/post/PopularPosts.js
+++ components/post/PopularPosts.js
@@ -1,13 +1,15 @@
 import BigCard from '@/components/post/BigCard'
 import SmallCards from '@/components/post/SmallCards'
 import { POPULAR_POST_LENGTH } from '@/constants'
+import PostCards from './PostCards'
 
 export default function PopularPosts({ extendClass = '', data }) {
   if (!data || data === null || data.length < POPULAR_POST_LENGTH) return <></>
 
   return (
     <>
-      <div className={`flex flex-wrap -mx-5 ${extendClass}`}>
+      {/* Desktop */}
+      <div className={`hidden lg:flex flex-wrap -mx-5 ${extendClass}`}>
         <div className="w-full px-5 lg:w-5/12 mb-5 md:mb-10 lg:mb-0">
           <BigCard data={data[0]} />
         </div>
@@ -15,6 +17,9 @@ export default function PopularPosts({ extendClass = '', data }) {
           <SmallCards data={data} />
         </div>
       </div>
+
+      {/* Mobile-Tablet */}
+      <PostCards posts={data} />
     </>
   )
 }
