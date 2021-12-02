0258c55fbadb7c7965a8930269642f424b8798e1
Faris Han <farishanugraha@gmail.com>
2021-12-02 22:30:29 +0700

    Fix SEO

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,22 @@ import 'tailwindcss/tailwind.css'
 import NextNprogress from 'nextjs-progressbar'
 import 'react-image-lightbox/style.css'
 import '../index.css'
+import useContent from '@/helpers/use-content'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
+  const { site_title } = useContent()
+
   return (
     <>
+      <Head>
+        <title>{site_title}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Sekolah bakat non-formal dan non-komersial yang ditujukan untuk mengajarkan ilmu sesuai minat dan bakat peserta didik."
+        />
+      </Head>
       <NextNprogress
         color="#D2292D"
         startPosition={0.3}
--- pages/_document.js
+++ pages/_document.js
@@ -11,10 +11,30 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&family=Poppins&display=swap"
             rel="stylesheet"
+          />
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-T2PBCVB973"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-T2PBCVB973');
+            `
+            }}
           />
         </Head>
         <body className="font-body">
